99fbd3b5a2039fb46a70ed69931b0d6fa8f37024
ukaoskid <ukaoskid@users.noreply.github.com>
2021-12-14 22:02:03 +0000

    Resolve eslint violations

--- src/server.ts
+++ src/server.ts
@@ -9,16 +9,16 @@ import { SwaggerFactory } from './factory/swagger-factory';
 import { fallback } from './middlewares/_index';
 import { pushHttpEvents } from './middlewares/_index';
 import { errorHandler, logError } from './middlewares/error-handler.middleware';
+import { guardExecutor } from './middlewares/guard.middleware';
 import { ServerOptions } from './models/_index';
 import { Controllers } from './models/dependency-injection/controller.service';
+import { Injector } from './models/dependency-injection/injector.service';
 import { Modules } from './models/dependency-injection/module.service';
+import { CanExecute } from './models/interfaces/authorization.interface';
 import { LifeCycleService } from './services/life-cycle/life-cycle.service';
 import { LoggerService } from './services/logger/logger.service';
 import { destroyInjectables, loadInjectables } from './utils/dependencies.utils';
 import { getPath } from './utils/fs.utils';
-import { guardExecutor } from './middlewares/guard.middleware';
-import { Injector } from './models/dependency-injection/injector.service';
-import { CanExecute } from './models/interfaces/authorization.interface';
 
 export interface GlobalMiddlewares {
   preRoutes?: any[];
