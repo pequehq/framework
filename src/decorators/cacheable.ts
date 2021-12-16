24f101a0ba248061396c0e3939d6a72a68099cd9
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-12-16 18:06:54 +0100

    wip

--- src/decorators/cacheable.ts
+++ src/decorators/cacheable.ts
@@ -2,27 +2,28 @@ import { Injector } from '../models/dependency-injection/injector.service';
 import { CacheManager } from '../models/interfaces/cache/cache-client.abstract';
 
 interface CacheOptions {
-  key: string | (() => void);
-  server?: string;
+  key: string | (() => string);
   ttl?: number;
 }
 
-export const Cacheable = (options: CacheOptions) => {
-  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+export function Cacheable(options: CacheOptions): MethodDecorator {
+  return <T>(target, propertyKey, descriptor): TypedPropertyDescriptor<T> => {
     const originalMethod = descriptor.value;
+
     descriptor.value = async (...args) => {
       const cacheService = Injector.resolve<CacheManager>('CacheService');
       const key = typeof options.key === 'function' ? options.key.apply(this, args) : options.key;
       const cache = await cacheService.get(key);
+
       if (cache) {
         return cache;
-      } else {
-        const original = await Promise.resolve(originalMethod.apply(this, args));
-        await cacheService.set(key, original, 0);
-        return original;
       }
+
+      const original = await Promise.resolve(originalMethod.apply(this, args));
+      cacheService.set(key, original, options.ttl);
+      return original;
     };
 
     return descriptor;
   };
-};
+}
