947060e1e16e331c190881616ac078b87f70c6c7
Simone Di Cicco <simone.dicicco@aptar.com>
2021-12-17 22:51:23 +0100

    chore: Guard decorator fixed + Swagger DTO args type fixed + Sample fixed

--- src/decorators/authorization.ts
+++ src/decorators/authorization.ts
@@ -3,10 +3,8 @@ import { ControllerDefinition } from '../models/controller-definition.interface'
 import { RouteDefinition } from '../models/interfaces/route-definition.interface';
 import { GuardClass } from '../models/interfaces/types';
 
-type MethodOrClassDecorator = MethodDecorator;
-
-export const Guard = (guard: GuardClass): MethodOrClassDecorator => {
-  return (target, propertyKey, descriptor): void => {
+export const Guard = (guard: GuardClass): MethodDecorator & ClassDecorator => {
+  return (target, propertyKey?, descriptor?): void => {
     const isClassDecorator = !descriptor;
 
     if (isClassDecorator) {
@@ -15,14 +13,16 @@ export const Guard = (guard: GuardClass): MethodOrClassDecorator => {
       Reflect.defineMetadata(DECORATORS.metadata.CONTROLLER, controller, target);
     }
 
-    const routes: RouteDefinition[] = Reflect.getMetadata(DECORATORS.metadata.ROUTES, target.constructor);
+    const routes: RouteDefinition[] = Reflect.getMetadata(DECORATORS.metadata.ROUTES, target.constructor) || [];
 
-    if (routes[routes.length - 1].guards) {
-      // @TODO check why ts complains if optional chaining operator is removed
-      // since the value has been asserted as not undefined in the previous statement
-      routes[routes.length - 1].guards?.push(guard);
-    } else {
-      routes[routes.length - 1].guards = [guard];
+    if (routes.length > 0) {
+      if (routes[routes.length - 1].guards) {
+        // @TODO check why ts complains if optional chaining operator is removed
+        // since the value has been asserted as not undefined in the previous statement
+        routes[routes.length - 1].guards?.push(guard);
+      } else {
+        routes[routes.length - 1].guards = [guard];
+      }
     }
 
     Reflect.defineMetadata(DECORATORS.metadata.ROUTES, routes, target.constructor);
