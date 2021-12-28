9ce9fcb781ddae6ddae304f293a5ca1210678f86
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-12-28 22:21:10 +0100

    add scheduler test

---
+++ src/decorators/scheduler.test.ts
@@ -0,0 +1,30 @@
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
+
+import { DECORATORS } from '../models/constants/decorators';
+import { SchedulerService } from '../services/scheduler/scheduler.service';
+import { Scheduler, SchedulerConfig } from './scheduler';
+
+const test = suite('Scheduler');
+
+test('should contain the right Scheduler metadata', async () => {
+  class TestClass {
+    @Scheduler('testEvent', '*/5 * * * * *')
+    testMethod() {
+      // empty
+    }
+  }
+
+  const metadata: Map<string, SchedulerConfig> =
+    Reflect.getMetadata(DECORATORS.metadata.SCHEDULER, SchedulerService) ?? new Map<string, SchedulerConfig>();
+
+  assert.is(metadata.size, 1);
+  assert.ok(metadata.has('TestClass_testMethod'));
+  assert.equal(metadata.get('TestClass_testMethod'), {
+    name: 'testEvent',
+    cron: '*/5 * * * * *',
+    listener: new TestClass().testMethod,
+  });
+});
+
+test.run();
