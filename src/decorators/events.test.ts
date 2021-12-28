147fbd325f75628719fcee352f45ef7dddad146e
Simone Di Cicco <simone.dicicco@gmail.com>
2021-12-28 09:50:50 +0100

    test: add OnEvent test (#32)
    * chore: add events test wip
    
    * chore: events test wip
    
    * chore: add EventStorage service and OnEvent test added
    
    * update coverage badge
    
    * chore: add after each cleanup for OnEvent test
    
    * update coverage badge
    
    * chore: add EventStorage test
    
    * update coverage badge
    
    Co-authored-by: ukaoskid <ukaoskid@users.noreply.github.com>

---
+++ src/decorators/events.test.ts
@@ -0,0 +1,31 @@
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
+
+import { EventStorage, OnEventInterface } from '../services/events/event-storage.service';
+import { loadInjectables } from '../utils/dependencies.utils';
+import { OnEvent } from './events';
+
+const test = suite('Events');
+
+test.before(async () => {
+  await loadInjectables();
+});
+
+test.after.each(async () => {
+  EventStorage.remove('testEvent');
+});
+
+test('should set event listener metadata', () => {
+  class TestClass {
+    @OnEvent('testEvent')
+    testMethod(): string {
+      return 'value';
+    }
+  }
+
+  const events: OnEventInterface[] = EventStorage.get('testEvent');
+  assert.is(events.length, 1);
+  assert.is(events[0].listener(), 'value');
+});
+
+test.run();
---
+++ src/services/events/event-storage.service.ts
@@ -0,0 +1,37 @@
+import { Listener, NativeEventsType } from '../../models';
+
+export interface OnEventInterface {
+  event: string | NativeEventsType;
+  listener: (...args: unknown[]) => void;
+}
+
+class EventStorageService {
+  private listeners = new Map<string, Listener[]>();
+
+  add(event: string, listener: Listener): void {
+    const eventListeners = this.listeners.get(event) ?? [];
+    eventListeners.push(listener);
+    this.listeners.set(event, eventListeners);
+  }
+
+  remove(event: string): void {
+    this.listeners.delete(event);
+  }
+
+  get(event: string): OnEventInterface[] {
+    const events: OnEventInterface[] = [];
+    const listeners = this.listeners.get(event) ?? [];
+    listeners.forEach((listener) => events.push({ event, listener }));
+    return events;
+  }
+
+  getAll(): OnEventInterface[] {
+    const events: OnEventInterface[] = [];
+    this.listeners.forEach((listeners, event) => {
+      listeners.forEach((listener) => events.push({ event, listener }));
+    });
+    return events;
+  }
+}
+
+export const EventStorage = new EventStorageService();
