c8a727ef0a1a4ce3f4b17a3e46ac03d42d4805d6
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2021-12-29 15:09:25 +0100

    chore: move config set to Server

--- src/factory/express-factory.ts
+++ src/factory/express-factory.ts
@@ -1,9 +1,7 @@
 import http from 'http';
 
 import { ServerOptions } from '../models';
-import { CONFIG_STORAGES } from '../models/constants/config';
 import { Server } from '../server';
-import { Config } from '../services/config/config.service';
 import { LifeCycleService } from '../services/life-cycle/life-cycle.service';
 import { isMaster, setupWorkers } from '../utils/cluster.utils';
 
@@ -11,8 +9,6 @@ export class ExpressFactory {
   private static expressServer: http.Server;
 
   static createServer = async (options: ServerOptions): Promise<http.Server> => {
-    Config.set(CONFIG_STORAGES.EXPRESS_SERVER, options);
-
     if (options.isCpuClustered && isMaster()) {
       setupWorkers();
       return ExpressFactory.expressServer;
