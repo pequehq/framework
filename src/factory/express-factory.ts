0c746196c624c4236e2392942c005a420537dd7d
Simone Di Cicco <simone.dicicco@aptar.com>
2021-12-29 13:33:22 +0100

    chore: move all that regards Server from ExpressFactory to Server + Config service creation and removal of ExpressFactory reference where not needed

--- src/factory/express-factory.ts
+++ src/factory/express-factory.ts
@@ -1,18 +1,17 @@
 import http from 'http';
-import { Socket } from 'net';
 
 import { ServerOptions } from '../models';
+import { CONFIG_STORAGES } from '../models/constants/config';
 import { Server } from '../server';
+import { Config } from '../services/config/config.service';
 import { LifeCycleService } from '../services/life-cycle/life-cycle.service';
 import { isMaster, setupWorkers } from '../utils/cluster.utils';
 
 export class ExpressFactory {
-  private static sharedOptions: ServerOptions;
   private static expressServer: http.Server;
-  private static sockets = new Set<Socket>();
 
   static createServer = async (options: ServerOptions): Promise<http.Server> => {
-    ExpressFactory.sharedOptions = options;
+    Config.set(CONFIG_STORAGES.EXPRESS_SERVER, options);
 
     if (options.isCpuClustered && isMaster()) {
       setupWorkers();
@@ -20,48 +19,10 @@ export class ExpressFactory {
     }
 
     await LifeCycleService.triggerServerBootstrap();
-
     const server = new Server(options);
-    const app = await server.bootstrap();
-    const port = options.port || 8888;
-    const hostname = options.hostname || 'localhost';
+    await server.bootstrap();
 
-    await LifeCycleService.triggerServerListen();
-
-    ExpressFactory.expressServer = app.listen(port, hostname, async () => {
-      server.logger().log({ level: 'debug', data: `Server is running @${hostname}:${port}` });
-      server.logger().log({ level: 'debug', data: `CPU Clustering is ${options.isCpuClustered ? 'ON' : 'OFF'}` });
-      await LifeCycleService.triggerServerStarted();
-    });
-
-    // Connections management.
-    ExpressFactory.expressServer.on('connection', (socket) => {
-      ExpressFactory.expressServer.once('close', () => ExpressFactory.sockets.delete(socket));
-      ExpressFactory.sockets.add(socket);
-    });
-
+    ExpressFactory.expressServer = server.getServer();
     return ExpressFactory.expressServer;
   };
-
-  static getServerOptions(): ServerOptions {
-    return ExpressFactory.sharedOptions;
-  }
-
-  static async closeServer(): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      // Ending all the open connections first.
-      for (const socket of ExpressFactory.sockets) {
-        socket.destroy();
-        ExpressFactory.sockets.delete(socket);
-      }
-
-      ExpressFactory.expressServer.close((err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(true);
-        }
-      });
-    });
-  }
 }
---
+++ src/models/constants/config.ts
@@ -0,0 +1,3 @@
+export const CONFIG_STORAGES = {
+  EXPRESS_SERVER: 'express-server',
+};
