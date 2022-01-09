930e51d6e352aa9ee93f98175ac89c869312edfc
Simone Di Cicco <simone.dicicco@aptar.com>
2022-01-09 15:50:09 +0100

    chore: restore peque factory merge misses

--- src/factory/peque-factory.ts
+++ src/factory/peque-factory.ts
@@ -1,25 +1,87 @@
 import http from 'http';
 
-import { ServerOptions } from '../models';
+import { MicroserviceClass, ServerOptions } from '../models';
+import { Controllers } from '../models/dependency-injection/controller.service';
+import { Modules } from '../models/dependency-injection/module.service';
+import { Providers } from '../models/dependency-injection/provider.service';
+import { WebSockets } from '../models/dependency-injection/websockets.service';
 import { Server } from '../server';
+import { TransportQueue } from '../services';
 import { LifeCycleManager } from '../services/life-cycle/life-cycle.service';
+import { Microservices } from '../services/microservice/microservice.service';
+import { Gateways } from '../services/microservice/microservice-gateway.service';
 import { clusterUtils } from '../utils/cluster.utils';
+import { destroyProviders, loadProviders } from '../utils/dependencies.utils';
 
 export class PequeFactoryService {
-  private expressServer: http.Server;
+  private server: Server;
+  private initialized = false;
+
+  private async init(): Promise<void> {
+    if (this.initialized) {
+      return;
+    }
+
+    const terminationSignals = ['SIGINT', 'SIGTERM', 'SIGBREAK', 'SIGHUP'];
+    terminationSignals.forEach((element) => {
+      process.on(element, async () => {
+        await this.terminator();
+      });
+    });
+
+    process.on('uncaughtException', async (error) => {
+      await LifeCycleManager.triggerUncaughtException(error);
+      console.error(error);
+      process.exit(0);
+    });
+
+    process.on('unhandledRejection', async (error: string) => {
+      await LifeCycleManager.triggerUncaughtRejection(new Error(error));
+      console.error(error);
+      process.exit(0);
+    });
+
+    TransportQueue.init();
+    Gateways.startListening();
+    await loadProviders();
+    this.initialized = true;
+  }
+
+  private async terminator(): Promise<void> {
+    await Controllers.destroyControllers();
+    await WebSockets.destroyWebSockets();
+    await Modules.destroyModules();
+    await destroyProviders();
+
+    await LifeCycleManager.triggerServerListenStop();
+    await this.server.closeServer();
+    Gateways.stopListening();
+
+    await LifeCycleManager.triggerServerShutdown();
+    Providers.unsetAll();
+
+    process.exit(1);
+  }
 
   async createServer(options: ServerOptions): Promise<http.Server> {
     if (options.isCpuClustered && clusterUtils.isMaster()) {
       clusterUtils.setupWorkers();
-      return this.expressServer;
+      return this.server.getServer();
     }
+
+    await this.init();
 
     await LifeCycleManager.triggerServerBootstrap();
     const server = new Server(options);
     await server.bootstrap();
 
-    this.expressServer = server.getServer();
-    return this.expressServer;
+    this.server = server;
+    return server.getServer();
+  }
+
+  async createMicroservices(services: { services: MicroserviceClass[] }): Promise<void> {
+    await this.init();
+    await Microservices.startMicroservices();
   }
 }
 
