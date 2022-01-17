bdc18ffe08263ed2007030934ea94574172f9c34
ukaoskid <ukaoskid@users.noreply.github.com>
2022-01-17 20:07:47 +0000

    resolve style guide violations

--- packages/di/src/decorators/inject.decorator.ts
+++ packages/di/src/decorators/inject.decorator.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 
-import { META_INJECT } from '../models/constants/metadata.constants';
 import { IProviderInject } from '../models';
 import { CONTAINER_INJECTABLE } from '../models/constants/containers.constants';
+import { META_INJECT } from '../models/constants/metadata.constants';
 
 export function Inject(options: IProviderInject): PropertyDecorator & ParameterDecorator {
   return (target, propertyKey, parameterIndex?) => {
