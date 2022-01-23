1985bf018f0e71555851fb999255d53304a6c95f
Simone Di Cicco <simone.dicicco@aptar.com>
2022-01-23 17:35:27 +0100

    feat: smb wip

--- packages/smb-server/src/di.ts
+++ packages/smb-server/src/di.ts
@@ -2,8 +2,8 @@ import { DiContainer } from 'peque-di';
 import { CommandEmitter, CommandReceiver, EventService } from 'peque-smb-commons/src';
 import { Command, CommandParser } from 'ultra-runner/lib/parser';
 
+import { MessageCommand, PublishCommand, SubscribeCommand, WelcomeCommand } from './commands';
 import { Broker } from './server/broker.class';
-import { SubscribeCommand } from './commands';
 
 export const DI = new DiContainer();
 
@@ -12,5 +12,8 @@ DI.set(CommandParser, CommandParser.name);
 DI.set(CommandReceiver, CommandReceiver.name);
 DI.set(CommandEmitter, CommandEmitter.name);
 DI.set(Command, Command.name);
+DI.set(WelcomeCommand, WelcomeCommand.name);
 DI.set(SubscribeCommand, SubscribeCommand.name);
+DI.set(MessageCommand, MessageCommand.name);
+DI.set(PublishCommand, PublishCommand.name);
 DI.set(Broker, Broker.name);
