e3568eec42fce9dc2ed2c57bdd2aeae468deac51
Simone Di Cicco <simone.dicicco@gmail.com>
2022-01-30 21:19:15 +0100

    chore: lifecycle to on event + tests

--- packages/framework/src/services/life-cycle/life-cycle.service.ts
+++ packages/framework/src/services/life-cycle/life-cycle.service.ts
@@ -1,4 +1,4 @@
-import { ClassDeclaration } from '../../models';
+import { ClassDeclaration, LifeCycleEvent, LifeCycleEventsType } from '../../models';
 import { getAllInstances } from '../../utils/instances.utils';
 import { Subjects } from '../subjects/subjects';
 
@@ -16,46 +16,49 @@ class LifeCycleManagerService {
     }
   }
 
-  #pushEvent(event: string, data = {}): void {
-    Subjects.lifeCycleSubject.next({ event: { event, transport: 'internal' }, data });
+  #pushEvent(event: LifeCycleEventsType, data = {}): void {
+    const lifeCycleData: LifeCycleEvent = { event, data };
+    Subjects.lifeCycleSubject.next({ event: { event: 'lifecycle', transport: 'internal' }, data: lifeCycleData });
   }
 
   async triggerProviderInit(instance: InstanceType<ClassDeclaration>): Promise<void> {
-    this.#pushEvent('lifecycle.init.provider');
+    await this.#triggerLifeCycleEvent(instance, 'onProviderInit');
+    this.#pushEvent('lifecycle.init.provider', { instance: instance.constructor.name });
   }
 
   async triggerProviderDestroy(instance: InstanceType<ClassDeclaration>): Promise<void> {
-    this.#pushEvent('lifecycle.destroy.provider');
+    await this.#triggerLifeCycleEvent(instance, 'onProviderDestroy');
+    this.#pushEvent('lifecycle.destroy.provider', { instance: instance.constructor.name });
   }
 
   async triggerModuleInit(instance: InstanceType<ClassDeclaration>): Promise<void> {
     await this.#triggerLifeCycleEvent(instance, 'onModuleInit');
-    this.#pushEvent('lifecycle.init.module');
+    this.#pushEvent('lifecycle.init.module', { instance: instance.constructor.name });
   }
 
   async triggerModuleDestroy(instance: InstanceType<ClassDeclaration>): Promise<void> {
     await this.#triggerLifeCycleEvent(instance, 'onModuleDestroy');
-    this.#pushEvent('lifecycle.destroy.module');
+    this.#pushEvent('lifecycle.destroy.module', { instance: instance.constructor.name });
   }
 
   async triggerWebSocketsInit(instance: InstanceType<ClassDeclaration>): Promise<void> {
     await this.#triggerLifeCycleEvent(instance, 'onWebSocketInit');
-    this.#pushEvent('lifecycle.init.websocket');
+    this.#pushEvent('lifecycle.init.websocket', { instance: instance.constructor.name });
   }
 
   async triggerWebSocketsDestroy(instance: InstanceType<ClassDeclaration>): Promise<void> {
     await this.#triggerLifeCycleEvent(instance, 'onWebSocketDestroy');
-    this.#pushEvent('lifecycle.destroy.websocket');
+    this.#pushEvent('lifecycle.destroy.websocket', { instance: instance.constructor.name });
   }
 
   async triggerControllerInit(instance: InstanceType<ClassDeclaration>): Promise<void> {
     await this.#triggerLifeCycleEvent(instance, 'onControllerInit');
-    this.#pushEvent('lifecycle.init.controller');
+    this.#pushEvent('lifecycle.init.controller', { instance: instance.constructor.name });
   }
 
   async triggerControllerDestroy(instance: InstanceType<ClassDeclaration>): Promise<void> {
     await this.#triggerLifeCycleEvent(instance, 'onControllerDestroy');
-    this.#pushEvent('lifecycle.destroy.controller');
+    this.#pushEvent('lifecycle.destroy.controller', { instance: instance.constructor.name });
   }
 
   async triggerServerStarted(): Promise<void> {
