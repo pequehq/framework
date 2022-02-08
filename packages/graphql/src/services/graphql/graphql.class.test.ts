a9e06a64bf0bedb8cab373913a04974106f75bfa
Simone Di Cicco <simone.dicicco@gmail.com>
2022-02-08 07:30:42 +0100

    chore: gql wip

--- packages/graphql/src/services/graphql/graphql.class.test.ts
+++ packages/graphql/src/services/graphql/graphql.class.test.ts
@@ -1,4 +1,6 @@
+import { ApolloError } from 'apollo-server-express';
 import express from 'express';
+import { createServer } from 'http';
 import * as sinon from 'sinon';
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
@@ -19,7 +21,24 @@ test('should apply the schema to the app', async (context) => {
   const graphQL = new GraphQL();
   const app = express();
 
-  await graphQL.apply(__dirname + '/../../../test/schema/schema.graphql', app, '/graphql');
+  const ServiceResolvers = {
+    Query: {
+      helloWorld: (st: string) => {
+        try {
+          return 'pezzodimmerda ' + st;
+        } catch (error) {
+          throw new ApolloError(error);
+        }
+      },
+      sbeffo: () => ({ name: 'sbeffolino' }),
+    },
+  };
+
+  await graphQL.apply(__dirname + '/../../../test/schema/schema.graphql', ServiceResolvers, app, '/graphql');
+  // const httpServer = createServer(app);
+  // httpServer.listen({ port: 3000 }, (): void =>
+  //   console.log(`GraphQL-Server is running on http://localhost:3000/graphql`),
+  // );
   assert.is(1, 1);
 });
 
--- packages/graphql/src/services/graphql/graphql.class.ts
+++ packages/graphql/src/services/graphql/graphql.class.ts
@@ -2,12 +2,18 @@ import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { loadSchemaSync } from '@graphql-tools/load';
 import { ApolloServer } from 'apollo-server-express';
 import { Application } from 'express';
+import { makeExecutableSchema } from 'graphql-tools';
+import { IResolvers } from '@graphql-tools/utils';
 
 export class GraphQL {
-  async apply(schema: string, app: Application, path: string): Promise<void> {
-    const gqlSchema = loadSchemaSync(schema, { loaders: [new GraphQLFileLoader()] });
+  async apply(schemaPath: string, resolvers: IResolvers | IResolvers[], app: Application, path: string): Promise<void> {
+    const typeDefs = loadSchemaSync(schemaPath, { loaders: [new GraphQLFileLoader()] });
+    const schema = makeExecutableSchema({
+      typeDefs,
+      resolvers,
+    });
     const server = new ApolloServer({
-      schema: gqlSchema,
+      schema,
     });
 
     await server.start();
